5c7ce2167252b018b81a2a6bde81c6b01a11710f
BaeEJ <flavormint0124@gmail.com>
2020-11-18 16:36:17 +0900

    Scrolltop
    슬라이드 스크롤 넘어가는 기능 추가

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 $(function(){
-  //script 입력영역
-});
+  // $("nav ul li").each(function(){
+  //   var num = $(this).find("a");
+  //   console.log($(num.hash))
+  // })
+
+  $("nav ul li a, .back_to_top a, .footer_top a,a.button").click(function(e){
+
+    var thisElem = $(this.hash);
+    $("html,body").stop();
+    $("html,body").animate({scrollTop : thisElem.offset().top},1500);
+    // e.preventDefault();
+    return false;
+  });//clickfunction
+});//document ready
